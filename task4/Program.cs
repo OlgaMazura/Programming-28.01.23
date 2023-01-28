1c175c88194bb5eebb8ca0de3e7141a36962b925
OlgaMazura <ofmazura@gmail.com>
2023-01-28 12:33:55 +0100

    task4 - done

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Enter posive number: ");
+int N = int.Parse(Console.ReadLine()!);
+int K = 1;
+while (K <= N)
+{
+    if(K %2 == 0)
+    {
+        Console.Write($"{K} "); 
+    }
+        K += 1; 
+}
